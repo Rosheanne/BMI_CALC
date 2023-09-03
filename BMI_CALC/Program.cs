97dee2fc563e2cbc9a0e6fddd5be201f27ef3edb
theoshi21 <kelvinignacio2110@gmail.com>
2023-09-03 22:30:10 +0800

    changes to format

--- BMI_CALC/Program.cs
+++ BMI_CALC/Program.cs
@@ -10,7 +10,7 @@ namespace MP3_1_Ignacio
     {
         static void Main(string[] args)
         {
-            float weight, height, bBmi, bmi;
+            float weight, height, bmi;
 
             Console.WriteLine("----BMI CALCULATOR----");
             Console.Write("Weight (kg): ");
@@ -18,29 +18,27 @@ namespace MP3_1_Ignacio
             Console.Write("Height (m): ");
             height = float.Parse(Console.ReadLine());
 
-            bBmi = (weight / (height * height));
-            bmi = String.Format("{0:0.00}", bmi);
-
+            bmi = (weight / (height * height));
 
             if (bmi < 18.5)
             {
-                Console.WriteLine($"Your BMI ({bmi}) is underweight.");
+                Console.WriteLine($"Your BMI  \"+String.Format(\"{{0:0.00}}\", bmi)+\") is underweight.");
             }
             else if(bmi >= 18.5 && bmi < 25)
             {
-                Console.WriteLine($"Your BMI ({bmi}) is normal.");
+                Console.WriteLine($"Your BMI \"+String.Format(\"{{0:0.00}}\", bmi)+\") is normal.");
             }
             else if(bmi >= 25 && bmi < 30)
             {
-                Console.WriteLine($"Your BMI ({bmi}) is overweight.");
+                Console.WriteLine($"Your BMI \"+String.Format(\"{{0:0.00}}\", bmi)+\") is overweight.");
             }
             else if(bmi >= 30 && bmi < 40)
             {
-                Console.WriteLine($"Your BMI ({bmi}) is obese.");
+                Console.WriteLine($"Your BMI \"+String.Format(\"{{0:0.00}}\", bmi)+\") is obese.");
             }
             else if(bmi >= 40)
             {
-                Console.WriteLine($"Your BMI ({bmi}) is extremely obese.");
+                Console.WriteLine($"Your BMI \"+String.Format(\"{{0:0.00}}\", bmi)+\") is extremely obese.");
             }
             else
             {
